c94834efe0ef368e6358bb26b4088da75cbc86d9
jvbutter <62125415+jvbutter@users.noreply.github.com>
2023-06-02 23:42:06 +0000

    Latest build of target web.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,40 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "   example.org   https:\/\/example.org   copyright  "
+},
+{
+  "id": "section-title",
+  "level": "1",
+  "url": "section-title.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "In Codespaces",
+  "body": " In Codespaces  I can build to web, and view the result.  I can build to latex , and view the result.  I cannot build to PDF, however.  "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
